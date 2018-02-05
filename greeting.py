582aca2980939cba47288effd789031205403a5b
Johnfra797 <johnfra797@hotmail.com>
2018-02-05 09:14:30 -0500

    first commit

---
+++ greeting.py
@@ -0,0 +1,7 @@
+
+class greeting:
+    def __init__(self,name):  # Initialize when created
+        self.name=name
+
+    def __greetng__(self):
+        print('Hello %s ,how are you?'% (self.name))
---
+++ main.py
@@ -0,0 +1,11 @@
+from greeting import greeting
+
+class main:
+    def __init__(self):  # Initialize when created
+        print('Run main')
+        greetng = greeting('Paul')
+        greetng.__greetng__()
+
+
+mn=main()
+
